5e35d7fbae6870299ae5f29f71ee65181cc74df3
Thomas Herr <thom1810@gmail.com>
2017-09-20 17:27:55 -0400

    Added url output message to question6 script

--- Question6.py
+++ Question6.py
@@ -1,6 +1,7 @@
 # Raw data from http://www.pythonchallenge.com/pc/def/banner.p
 from urllib.request import urlopen
 import pickle
+import url
 
 data = pickle.load(urlopen("http://www.pythonchallenge.com/pc/def/banner.p"))
 
@@ -14,3 +15,5 @@ for line in data:
         print(each_item[0]*each_item[1], end='')
     # After each item in the line has been processed print a newline
     print()
+
+print(url.format("channel"))
